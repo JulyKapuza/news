d994119ecb3b0586f3c3824d34223c5baec1f5f8
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-03-02 22:58:34 +0200

    final version

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,18 +1,13 @@
 import axios from "axios";
 
-
-// axios.defaults.baseURL = 'http://localhost:5000';
-axios.defaults.baseURL ="https://breaking-news.herokuapp.com/api"
+axios.defaults.baseURL = "https://apinews.herokuapp.com";
 
 export const fetchPosts = async (limit: number) => {
-  const { data } = await axios.get(
-    `/news?_limit=${limit}`
-  );
+  const { data } = await axios.get(`/news?_limit=${limit}`);
 
   return data;
 };
 
-
 export const deleteOneNews = async (id: string) => {
-    return await axios.delete(`/news/${id}`);
-  };
+  return await axios.delete(`/news/${id}`);
+};
