1d322b1cab61636800ce91fa901e0cfe78d06912
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-03-02 18:10:25 +0200

    server

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 
-axios.defaults.baseURL = 'http://localhost:5000';
+// axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL ="https://breaking-news.herokuapp.com/api"
 
 export const fetchPosts = async (limit: number) => {
   const { data } = await axios.get(
